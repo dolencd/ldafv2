bb93b99d6bd8fc1358ac0a9a05c9ff336ba8f7b0
dolencd <dejdolenc@gmail.com>
2020-01-10 17:56:08 +0200

    added hasher function for stress testing purposes

--- packages/Service/src/service/main.js
+++ packages/Service/src/service/main.js
@@ -1,6 +1,26 @@
+const crypto = require("crypto");
+
+const getHash = (data) => {
+    return crypto.createHash("sha256").update(data).digest();
+}
+
 const updateValue = (event, context, callback) => {
     context.storedValue = event.params.value;
     callback(context);
+}
+
+const hashData = (event, context, callback) => {
+    let {data, repetitions} = event.params;
+
+    data = Buffer.from(data, "hex")
+    for(let i = 0; i < repetitions; i++){
+        data = getHash(data);
+    }
+
+    callback(null, {
+        hash: data.toString("hex")
+    })
+
 }
 
 const add = (event, context, callback) => {
@@ -21,12 +41,13 @@ const add = (event, context, callback) => {
 const getBlockNumber = async (event, context, callback) => {
     let blockNumber = await event.web3.eth.getBlockNumber().catch(console.error);
 
-    callback(context, {
+    callback(null, {
         value: blockNumber
     });
 }
 
 module.exports = {
+    hashData,
     updateValue,
     add,
     getBlockNumber
